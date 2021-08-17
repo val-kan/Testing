aa7f574f2cd255504fb9d2145aa3b430a12abb5f
Val Kan <valeriy.kan@gmail.com>
2021-08-16 23:05:22 -0400

    unit testing my 1st project

--- src/test/java/unitTesting/unittest_human.java
+++ src/test/java/unitTesting/unittest_human.java
@@ -17,4 +17,16 @@ public class unittest_human {
         Assert.assertEquals(result,30);
     }
 
+    @Test
+    public void humantest_Human_ageIs12() {
+        //Given - arrange
+        Human youth =new Human("boy","male",12);
+
+        //When - Act
+        int result = youth.myage();
+
+        //Then - Assert (actual,expected)
+        Assert.assertEquals(result, 13);
+    }
+
 }
